5b66cf4cd65aca3896852871d110904ed6de3675
admmaalfurqonmgl-design <admmaalfurqonmgl@gmail.com>
2026-03-09 13:10:25 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+const API = "PASTE_API_URL_DISINI"
+
+let namaUser=""
+
+function login(){
+
+fetch(API,{
+method:"POST",
+body:JSON.stringify({
+
+action:"login",
+username:document.getElementById("username").value,
+password:document.getElementById("password").value
+
+})
+
+})
+
+.then(r=>r.json())
+.then(res=>{
+
+if(res.status){
+
+namaUser=res.nama
+
+document.getElementById("login").style.display="none"
+
+document.getElementById("menu").style.display="block"
+
+document.getElementById("namaUser").innerText=res.nama
+
+}
+
+else{
+
+alert("Login gagal")
+
+}
+
+})
+
+}
+
+function presensi(jenis){
+
+navigator.geolocation.getCurrentPosition(pos=>{
+
+fetch(API,{
+method:"POST",
+body:JSON.stringify({
+
+action:"presensi",
+nama:namaUser,
+jenis:jenis,
+jam:new Date().toLocaleTimeString(),
+lat:pos.coords.latitude,
+lng:pos.coords.longitude
+
+})
+
+})
+
+alert("Presensi berhasil")
+
+})
+
+}
+
+function izin(){
+
+let jenis = prompt("Jenis izin")
+
+let ket = prompt("Keterangan")
+
+fetch(API,{
+method:"POST",
+body:JSON.stringify({
+
+action:"izin",
+nama:namaUser,
+jenis:jenis,
+keterangan:ket
+
+})
+
+})
+
+alert("Izin terkirim")
+
+}
+
+function presensiKelas(){
+
+let mk = prompt("Mata Kuliah")
+
+let judul = prompt("Judul Pembelajaran")
+
+let status = prompt("Status hadir/izin/sakit/alpha")
+
+fetch(API,{
+method:"POST",
+body:JSON.stringify({
+
+action:"kelas",
+mk:mk,
+judul:judul,
+nama:namaUser,
+status:status,
+dosen:namaUser
+
+})
+
+})
+
+alert("Presensi kelas tersimpan")
+
+}
